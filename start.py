d31d6cb55365d610441f308235c6dd906b1403f8
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-24 17:42:18 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from os import system 
-system ("git clone https://GitHub.com/majid-nex/nextron-bot && cd userbot && python3 -m userbot")
+system ("git clone https://GitHub.com/majid-nex/nextron-bot && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
 system("python3 -m userbot")
