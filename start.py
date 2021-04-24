14c9f5f3d7142bf2955da1a503e4b2312a221204
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-24 17:32:44 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,6 @@
+from os import system 
+system ("git clone https://GitHub.com/majid-nex/nextron-bot && cd ULTRA-X && python3 -m userbot")
+# try af
+print("OH SYSTEM IS CRASHED")
+system("python3 -m userbot")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
