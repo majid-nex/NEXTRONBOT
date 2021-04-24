997b6043aa4f20fc7a810c8207f9a7516b0848c3
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-24 18:05:55 +0530

    Delete start.py

--- start.py
+++
@@ -1,6 +0,0 @@
-from os import system 
-system ("git clone https://GitHub.com/majid-nex/nextron-bot && python3 -m userbot")
-# try af
-print("OH SYSTEM IS CRASHED")
-system("python3 -m userbot")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
